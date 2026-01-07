5081437e5fa3ef4b86a0fb849c1a7a9652df2967
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-07 18:25:06 +0100

    feat: implementing https://atcoder.jp/contests/abc133/tasks/abc133_b

--- test.cpp
+++ test.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
-#include <algorithm>
+#include <cmath>
 
 int main()
 {
-    int n, x;
-    std::cin >> n >> x;
-    std::vector<int> m;
+    int n, d, c = 0;
+    std::cin >> n >> d;
+    std::vector<int> x[n];
     for (int i=0; i < n; i++)
     {
-        int x;
-        std::cin >> x;
-        m.push_back(x);
+        for (int j=0; j < d; j++)
+        {
+            int t; std::cin >> t; x[i].push_back(t);
+        }
     }
-    x -= std::accumulate(m.begin(), m.end(), 0);
-    auto it = std::min_element(m.begin(), m.end());
-    std::cout << n + x / *it;
+
+
+    for (int i=0; i < n - 1; i++)
+    {
+        for (int j=0; j < d; j++)
+        {
+            int ele = 0;
+            for (int k=1; k + i < n; k++)
+            {
+                ele += pow(std::abs(x[i][j] - x[i + k][j]), 2);
+            }
+            std::cout << ele << std::endl;
+        }
+    }
+    // std::cout << c;
     return (0);
 }
