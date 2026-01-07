39add9bf6603368658ee39bc7f29c68aa7950105
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-07 17:01:22 +0100

    feat: implement https://atcoder.jp/contests/abc095/tasks/abc095_b

--- test.cpp
+++ test.cpp
@@ -1,48 +1,21 @@
-// https://atcoder.jp/contests/abc113/tasks/abc113_b
-
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include <vector>
+#include <numeric>
+#include <algorithm>
 
 int main()
 {
-    int n, t, a;
-    std::cin >> n >> t >> a;
-    std::vector<int> h;
+    int n, x;
+    std::cin >> n >> x;
+    std::vector<int> m;
     for (int i=0; i < n; i++)
     {
-        int tmp;
-        std::cin >> tmp;
-        h.push_back(tmp);
+        int x;
+        std::cin >> x;
+        m.push_back(x);
     }
-
-    float com = (t - a) / 0.006;
-    int ret;
-    
-    // Liner search with using the lambda function. (O(N))
-    // auto it = std::min_element(h.begin(), h.end(), 
-    //     [com](int a, int b)
-    //     {
-    //         return std::abs(a - com) < std::abs(b - com);
-    //     });
-    // ret = std::distance(h.begin(), it);
-
-    // lower_bound (O(logN)) but i use sort (O(NlogN))
-    auto tmp = h;
-    int val;
-    std::sort(h.begin(), h.end());
-    auto it = std::lower_bound(h.begin(), h.end(), com);
-    if (it != h.begin() && it != h.end())
-    {
-        if (std::abs(com - *(it - 1)) < std::abs(com - *it))
-            val = *(it - 1);
-    }
-    else
-        val = *it;
-    auto iter = std::find(tmp.begin(), tmp.end(), val);
-    ret = std::distance(tmp.begin(), iter);
-
-    std::cout << ret + 1;
+    x -= std::accumulate(m.begin(), m.end(), 0);
+    auto it = std::min_element(m.begin(), m.end());
+    std::cout << n + x / *it;
     return (0);
 }
