9400d52729c9935d838d9947fba27d9db15f8753
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-14 13:31:54 +0100

    feat: make template.cpp

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i < n; i++)
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    return (0);
+}
