447d65cceba855471240e266ff38728b5031393a
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-15 13:47:20 +0100

    feat: change template

--- note/template.cpp
+++ note/template.cpp
@@ -1,11 +1,12 @@
 #define ll long long
 #include <bits/stdc++.h>
+using namespace std;
 #define rep(n) for (size_t i=0; i < n; i++)
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
     return (0);
 }
