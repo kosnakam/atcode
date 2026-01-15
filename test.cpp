4a4c4185cce29e7576fda89143c4463586916ae0
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-15 13:46:36 +0100

    feat: B_Snuke'sColoring2 https://atcoder.jp/contests/abc047/tasks/abc047_b

--- test.cpp
+++ test.cpp
@@ -1,15 +1,28 @@
-#define ll long long
 #include <bits/stdc++.h>
-#define rep(n) for (size_t i=0; i < n; ++i)
+using namespace std;
+#define rep(n) for (int i=0; i < n; ++i)
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    size_t n, m; std::cin >> n >> m;
-    std::vector<size_t> ab(2 * m);
-    rep(2 * m) std::cin >> ab[i];
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int w, h, n;
+    cin >> w >> h >> n;
+    int x_min = 0, x_max = w;
+    int y_min = 0, y_max = h;
+
     rep(n)
-        std::cout << std::count(ab.begin(), ab.end(), i + 1) << std::endl;
-    return (0);
+    {
+        int x, y, a;
+        cin >> x >> y >> a;
+        if (a == 1) x_min = max(x_min, x);
+        else if (a == 2) x_max = min(x_max, x);
+        else if (a == 3) y_min = max(y_min, y);
+        else if (a == 4) y_max = min(y_max, y);
+    }
+    int width = max(0, x_max - x_min);
+    int height = max(0, y_max - y_min);
+    cout << width * height << endl;
+    return 0;
 }
