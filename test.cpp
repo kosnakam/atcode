c62f7f422788c0f5a0fc55c3cafd31e4153b4024
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-01-15 14:43:03 +0100

    feat: B_TwoColorsCardGame https://atcoder.jp/contests/abc091/tasks/abc091_b

--- test.cpp
+++ test.cpp
@@ -6,23 +6,20 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int n, m;
+    cin >> n;
+    vector<string> s(n); rep(n) cin >> s[i];
+    cin >> m;
+    vector<string> t(m); rep(m) cin >> t[i];
 
-    int w, h, n;
-    cin >> w >> h >> n;
-    int x_min = 0, x_max = w;
-    int y_min = 0, y_max = h;
-
-    rep(n)
+    int ret = 0;
+    for (string str: s)
     {
-        int x, y, a;
-        cin >> x >> y >> a;
-        if (a == 1) x_min = max(x_min, x);
-        else if (a == 2) x_max = min(x_max, x);
-        else if (a == 3) y_min = max(y_min, y);
-        else if (a == 4) y_max = min(y_max, y);
+        int c=0;
+        rep(n) if (s[i] == str) ++c;
+        rep(m) if (t[i] == str) --c;
+        ret = max(ret, c);
     }
-    int width = max(0, x_max - x_min);
-    int height = max(0, y_max - y_min);
-    cout << width * height << endl;
+    cout << ret;
     return 0;
 }
